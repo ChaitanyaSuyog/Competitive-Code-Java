938e409a1b66ab19763350ae6be0966039a6a979
Chaitanya S <chaitanya.suyog@gmail.com>
2023-10-22 07:32:18 +0530

    Corrected program
    Changed for loop to iterator

--- src/Codeforces/SpellCheck.java
+++ src/Codeforces/SpellCheck.java
@@ -1,25 +1,28 @@
 //Problem 1722A
 package Codeforces;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
+import java.util.*;
 public class SpellCheck {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
-        ArrayList<Character> valid = new ArrayList<>(Arrays.asList('T', 'i', 'm', 'u', 'r'));
         ArrayList<String> sol = new ArrayList<>();
 
         for (int i = 0; i < t; i++) {
             int n = scan.nextInt();
             scan.nextLine();
             String s = scan.nextLine();
+            ArrayList<Character> valid = new ArrayList<>(Arrays.asList('T', 'i', 'm', 'u', 'r'));
 
             boolean status = false;
             for (int j = 0; j < s.length(); j++) {
                 if(valid.contains(s.charAt(j))) {
+                    //Remove element
+                    Iterator<Character> itr = valid.iterator();
+                    while (itr.hasNext()) {
+                        if (itr.next() == s.charAt(j))
+                            itr.remove();
+                    }
                     status = true;
                 }
                 else {
@@ -27,7 +30,7 @@ public class SpellCheck {
                     break;
                 }
             }
-            if (status && n == 5) {
+            if (status && s.length() == 5) {
                 sol.add("YES");
             }
             else
