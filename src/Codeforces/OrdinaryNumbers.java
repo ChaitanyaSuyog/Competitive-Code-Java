cd692780943f789d69d4b56bb15e8ad04cbc9489
Chaitanya S <chaitanya.suyog@gmail.com>
2023-10-29 17:27:04 +0530

    Commit

--- src/Codeforces/OrdinaryNumbers.java
+++ src/Codeforces/OrdinaryNumbers.java
@@ -1,23 +1,37 @@
 package Codeforces;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OrdinaryNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
+        ArrayList<Integer> sol = new ArrayList<>();
 
         for (int i = 0; i < t; i++) {
             int n = scan.nextInt();
-            int noOfOrdinary = 0;
+            int noOfOrdinary;
+            int cM;
 
             if (n < 10)
                 noOfOrdinary = n;
-            else
-
+            else if (n == 10)
+                noOfOrdinary = 9;
+            else {
+                cM = closestMultiple(n);
+                noOfOrdinary = 9 + (cM / 11);
+            }
+            sol.add(noOfOrdinary);
+        }
+        for (int s : sol
+             ) {
+            System.out.println(s);
         }
     }
     public static int closestMultiple(int n) {
-        
+        n = n + (11 / 2);
+        n = n - (n % 11);
+        return n;
     }
 }
