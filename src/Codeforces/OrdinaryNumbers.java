d882f2666c2c2b98744d0db75e19ca079c9af022
Chaitanya S <chaitanya.suyog@gmail.com>
2023-10-29 16:56:58 +0530

    Update OrdinaryNumbers.java

--- src/Codeforces/OrdinaryNumbers.java
+++ src/Codeforces/OrdinaryNumbers.java
@@ -1,2 +1,23 @@
-package Codeforces;public class OrdinaryNumbers {
+package Codeforces;
+
+import java.util.Scanner;
+
+public class OrdinaryNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = scan.nextInt();
+            int noOfOrdinary = 0;
+
+            if (n < 10)
+                noOfOrdinary = n;
+            else
+
+        }
+    }
+    public static int closestMultiple(int n) {
+        
+    }
 }
